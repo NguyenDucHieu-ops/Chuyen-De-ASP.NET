1d4fbcdd3ed049719397f112678bd0c80750867d
Duc Hieu <nguyenvamaimai@gmail.com>
2026-03-12 13:11:26 +0700

    first commit

---
+++ NguyenDucHieu_2123110416/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NguyenDucHieu_2123110416.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
