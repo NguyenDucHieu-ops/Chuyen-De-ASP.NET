5a96a284fc640cabbe4ec1376ff299b28a707b7a
Duc Hieu <nguyenvamaimai@gmail.com>
2026-03-12 13:54:28 +0700

    Create Product and Category Controller

--- NguyenDucHieu_2123110416/Controllers/WeatherForecastController.cs
+++ NguyenDucHieu_2123110416/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace NguyenDucHieu_2123110416.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "BAlmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
